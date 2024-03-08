ba682ee0cf9f68929f9c9ec32cb9b63f663a4791
Cheng Yang <yangzju@gmail.com>
2024-03-08 17:51:38 +0800

    Update constants.tsx
    use NEXT_PUBLIC_VERCEL_URL instead of VERCEL_URL to make vercel deployment working

--- frontend/app/utils/constants.tsx
+++ frontend/app/utils/constants.tsx
@@ -1,4 +1,4 @@
-const apiBasePath = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+const apiBasePath = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : `http://localhost:3000`;
 export const apiBaseUrl = `${apiBasePath}/api`;
